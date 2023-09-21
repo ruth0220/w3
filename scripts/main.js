684dd0bc7c0abe546d7d5ffd3f6d354d47c6c280
ruth0220 <124371720+ruth0220@users.noreply.github.com>
2023-09-21 12:04:42 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -18,12 +18,6 @@ myButton.onclick = function() {
 
 let myImage = document.querySelector('img');
 
-myImage.onclick = function() {
-  let mySrc = myImage.getAttribute('src');
-
-
-  const myImage = document.querySelector("img");
-
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
   if (mySrc === "images/firefox-icon.png") {
